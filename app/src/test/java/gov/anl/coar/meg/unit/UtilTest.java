8ea04cf2e559d878f7b659ab79a5705cbee77ffc
Greg Rehm <grehm87@gmail.com>
2016-02-29 20:06:15 -0800

    add additional test

--- app/src/test/java/gov/anl/coar/meg/unit/UtilTest.java
+++ app/src/test/java/gov/anl/coar/meg/unit/UtilTest.java
@@ -7,8 +7,17 @@ import org.mockito.Mock;
 import org.junit.Test;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.assertFalse;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+import gov.anl.coar.meg.Constants;
 import gov.anl.coar.meg.Util;
 
 /**
@@ -20,8 +29,29 @@ public class UtilTest {
     @Mock
     Context mMockContext;
 
+    public static final String TMP_DIR = "/tmp/";
+
+    private Context setupContext(){
+        File mockDir = new File(this.TMP_DIR);
+        when(mMockContext.getFilesDir()).thenReturn(mockDir);
+        return mMockContext;
+    }
+
     @Test
     public void validateDoesNotHaveKey_returnsFalse() {
-        assertFalse(new Util().validateDoesNotHaveKey(mMockContext));
+        Context mockContext = setupContext();
+        assertFalse(new Util().validateDoesNotHaveKey(mockContext));
+    }
+
+    @Test
+    public void validateDoesNotHaveKey_returnsTrue() throws IOException {
+        Context mockContext = setupContext();
+        String path = this.TMP_DIR + Constants.SECRETKEY_FILENAME;
+        File temp = new File(path);
+        temp.deleteOnExit();
+        FileOutputStream output = new FileOutputStream(temp);
+        output.write("".getBytes());
+        output.close();
+        assertTrue(new Util().validateDoesNotHaveKey(mockContext));
     }
 }
