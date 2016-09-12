b39804565ef35eaea01390d8ef1b9dd8ee406083
Greg Rehm <grehm87@gmail.com>
2016-09-12 16:58:38 -0700

    point back to grehm.us and adding perf timing output

--- app/src/main/java/gov/anl/coar/meg/service/GCMListenerService.java
+++ app/src/main/java/gov/anl/coar/meg/service/GCMListenerService.java
@@ -24,15 +24,21 @@ public class GCMListenerService extends GcmListenerService {
             Log.d(TAG, "message received from: " + from);
             Log.d(TAG, "message action: " + action);
             if (action.contains("decrypt")) {
+                long start = System.currentTimeMillis();
                 String messageId = data.getString("message_id");
                 MEGMessage msg = MEGMessage.getEncryptedFromServer(messageId, this, getApplication());
                 msg.performDecryptionFlow();
+                Log.e(TAG, "Decrypt time: " + String.valueOf(System.currentTimeMillis() - start));
             } else if (action.contains("encrypt")) {
+                long start = System.currentTimeMillis();
                 String messageId = data.getString("message_id");
                 MEGMessage msg = MEGMessage.getDecryptedFromServer(messageId, this, getApplication());
                 msg.performEncryptionFlow();
+                Log.e(TAG, "Encrypt time: " + String.valueOf(System.currentTimeMillis() - start));
             } else if (action.contains("revoke")) {
+                long start = System.currentTimeMillis();
                 Util.deleteAllFiles(this);
+                Log.e(TAG, "Revocation time: " + String.valueOf(System.currentTimeMillis() / 1000 - start));
             }
         } catch (Exception e) {
             e.printStackTrace();
